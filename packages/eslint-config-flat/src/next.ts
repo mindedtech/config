c9fef2bfe92d08d864be2e1974ac72ca5e4b7264
Elie Rotenberg <elie.rotenberg@gmail.com>
2024-11-17 13:17:12 +0100

    chore: update dependencies, fix eslint-config-flat exports

--- packages/eslint-config-flat/src/next.ts
+++ packages/eslint-config-flat/src/next.ts
@@ -1,23 +1,16 @@
-import { fixupConfigRules } from "@eslint/compat";
 import { FlatCompat } from "@eslint/eslintrc";
 
 import baseConfig from "./index.js";
 
 import type { FlatConfigArray } from "./index";
-import type { FixupConfigArray } from "@eslint/compat";
 
-const flatCompat = new FlatCompat({
+const compat = new FlatCompat({
   baseDirectory: import.meta.dirname,
 });
 
-const flatCompatConfig = fixupConfigRules([
-  ...flatCompat.extends(`next/recommended`),
-  ...flatCompat.extends(`next/core-web-vitals`),
-] as FixupConfigArray);
-
 const config: FlatConfigArray = [
   ...baseConfig,
-  ...flatCompatConfig,
+  ...compat.extends(`next/core-web-vitals`, `next/typescript`),
   {
     rules: {
       "@next/next/no-html-link-for-pages": 0,
